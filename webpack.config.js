8716902324f5b9f15fca7beaaa69bb77e5efca17
Sana <amalsk0905@yandex.ru>
2023-06-01 22:32:27 +0300

    1.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
-    clean: true
+    clean: true,
   },
-
   devtool: 'source-map',
-
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
   module: {
     rules: [
       {
