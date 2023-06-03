50cd98d596abd431d26e15edb7898e03255f3fb0
Sana <amalsk0905@yandex.ru>
2023-06-03 20:39:21 +0300

    2

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,29 +1,30 @@
-import SortView from '../view/sort';
-import EventListView from '../view/events-list';
-import EventItemView from '../view/event-item';
-import EditFormView from '../view/edit-point';
-import NewItemFormView from '../view/create-form';
+import SortView from '../view/sort-view';
+import EventListView from '../view/events-list-view';
+import EventItemView from '../view/event-item-view';
+import EditFormView from '../view/edit-point-view';
+import NewItemFormView from '../view/create-form-view';
 import { render } from '../render';
 
 
 export default class BoardPresenter {
   eventListComponent = new EventListView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, tripPointsModel}) {
     this.boardContainer = boardContainer;
+    this.tripPointsModel = tripPointsModel;
   }
 
-
   init() {
+    this.tripPoints = [...this.tripPointsModel.getTripPoints()];
+
     render(new SortView(), this.boardContainer);
     render(this.eventListComponent, this.boardContainer);
 
     render(new NewItemFormView(), this.eventListComponent.getElement());
-    render(new EventItemView(), this.eventListComponent.getElement());
     render(new EditFormView(), this.eventListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventItemView(), this.eventListComponent.getElement());
+    for (let i = 0; i < this.tripPoints.length; i++) {
+      render(new EventItemView({tripPoint: this.tripPoints[i]}), this.eventListComponent.getElement());
     }
   }
 }
